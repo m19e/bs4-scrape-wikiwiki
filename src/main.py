aa4b88b80d469743e4ae7ebf4c2373eba3d090c7
m19e <nexus.me7290@gmail.com>
2020-06-25 15:54:57 +0900

    Get card name

--- src/main.py
+++ src/main.py
@@ -1,13 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://news.yahoo.co.jp/"
+URL = "https://wikiwiki.jp/shinycolors/%E5%B0%8F%E5%AE%AE%E6%9E%9C%E7%A9%82"
 response = requests.get(URL, timeout=1)
 
 # textでunicode, contentでstr
-print(response.text)
+# print(response.text)
 
 soup = BeautifulSoup(response.text, 'lxml')
 
-for a_tag in soup.find_all('a'):
-    print(a_tag.get('href'))  # リンクを表示
+for h4_tag in soup.select("h4[id^='h4_content_']"):
+    print(h4_tag.text)
