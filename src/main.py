42b8163e87c0845f11ba8a482ab5e149331eb2b6
m19e <nexus.me7290@gmail.com>
2020-06-26 22:46:18 +0900

    Add func print list

--- src/main.py
+++ src/main.py
@@ -3,6 +3,10 @@ import urllib.parse
 from bs4 import BeautifulSoup
 
 
+def print_list(list):
+    [print(item) for item in list]
+
+
 def main():
     URL = "https://wikiwiki.jp/shinycolors/%E5%B0%8F%E5%AE%AE%E6%9E%9C%E7%A9%82"
     response = requests.get(URL, timeout=1)
@@ -11,7 +15,7 @@ def main():
     cards = list(map(lambda h4: {'name': h4.a.text, 'detail': urllib.parse.urljoin("https://wikiwiki.jp/", h4.a.get("href")), 'image': soup.select_one(
         "a[href='{0}']>img".format(h4.a.get("href"))).get("src")}, filter(lambda h4: h4.a != None, soup.select("h4[id^='h4_content_']"))))
 
-    [print(card) for card in cards]
+    print_list(cards)
 
 
 if __name__ == "__main__":
