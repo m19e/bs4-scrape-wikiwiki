cd610c3865a8558ae14f8028c3536290484d1d79
m19e <nexus.me7290@gmail.com>
2020-06-26 19:28:12 +0900

    Add main func and package process

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://wikiwiki.jp/shinycolors/%E5%B0%8F%E5%AE%AE%E6%9E%9C%E7%A9%82"
-response = requests.get(URL, timeout=1)
-soup = BeautifulSoup(response.text, 'lxml')
 
-cards = list(map(lambda h4: {'name': h4.a.text, 'detail': h4.a.get("href"), 'image': soup.select_one("a[href='{0}']>img".format(h4.a.get("href"))).get("src")}, filter(lambda h4: h4.a != None, soup.select("h4[id^='h4_content_']"))))
+def main():
+    URL = "https://wikiwiki.jp/shinycolors/%E5%B0%8F%E5%AE%AE%E6%9E%9C%E7%A9%82"
+    response = requests.get(URL, timeout=1)
+    soup = BeautifulSoup(response.text, 'lxml')
 
-print(type(cards))
+    cards = list(map(lambda h4: {'name': h4.a.text, 'detail': h4.a.get("href"), 'image': soup.select_one(
+        "a[href='{0}']>img".format(h4.a.get("href"))).get("src")}, filter(lambda h4: h4.a != None, soup.select("h4[id^='h4_content_']"))))
+
+    print(type(cards))
+
+
+if __name__ == "__main__":
+    main()
