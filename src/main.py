0611e2a375c4972eb3ff0fe1eddbcef98a8ee675
m19e <nexus.me7290@gmail.com>
2020-06-26 19:00:37 +0900

    Remove needless

--- src/main.py
+++ src/main.py
@@ -3,10 +3,6 @@ from bs4 import BeautifulSoup
 
 URL = "https://wikiwiki.jp/shinycolors/%E5%B0%8F%E5%AE%AE%E6%9E%9C%E7%A9%82"
 response = requests.get(URL, timeout=1)
-
-# textでunicode, contentでstr
-# print(response.text)
-
 soup = BeautifulSoup(response.text, 'lxml')
 
 cards = list(map(lambda h4: {'name': h4.a.text, 'detail': h4.a.get("href"), 'image': soup.select_one("a[href='{0}']>img".format(h4.a.get("href"))).get("src")}, filter(lambda h4: h4.a != None, soup.select("h4[id^='h4_content_']"))))
